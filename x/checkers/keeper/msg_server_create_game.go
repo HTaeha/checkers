b5cd00360aa89c07fb689278901b6f10fb3c7936
Taeha Hong <xogk39@gmail.com>
2022-02-23 03:15:10 +0900

    Implements createGame message

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -12,6 +14,31 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 
 	// TODO: Handling the message
 	_ = ctx
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	storedGame := types.StoredGame{
+		Index:   newIndex,
+		Creator: msg.Creator,
+		Game:    rules.New().String(),
+		Red:     msg.Red,
+		Black:   msg.Black,
+	}
+
+	// err := storedGame.Validate()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
 }
